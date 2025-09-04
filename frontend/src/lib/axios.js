6319b37ac20763a7f42bbdce93b7e97fe543233d
Rohit Vishwakarma <rohitvishwa9810@gmail.com>
2025-09-04 19:58:24 +0530

    some update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, // Important for cookies
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api",
+  withCredentials: true,
 });
